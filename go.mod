bc09c90a05c3360c5fbf87af859e605ea794f44d
musashi.sakamoto <Musashi.Sakamoto@cognizant.com>
2019-10-17 17:07:00 +0900

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Musashi-Sakamoto/go-kubernetes
+
+go 1.13
---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	query := r.URL.Query()
+	name := query.Get("name")
+	if name == "" {
+		name = "Guest"
+	}
+	log.Printf("Received request for %s\n", name)
+	w.Write([]byte(fmt.Sprintf("Hello, %s\n", name)))
+}
+
+func healthHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+}
+
+func readinessHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+}
+
+func main() {
+
+}
